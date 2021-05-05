36f4748f9c7b1e7a80f8a3f448b00e525a2918fc
Robert <robertdoneux@gmail.com>
2021-05-05 21:30:25 +0100

    WEB-73: Adds HomeCollectionCardText test

---
+++ src/pages/components/card/__tests__/HomeCollectionCardText.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import HomeCollectionCardText from '../HomeCollectionCardText'
+
+describe('Home Collection Card Text', () => {
+
+    it('should render correctly', () => {
+
+        const { getByTestId } = render(<HomeCollectionCardText title="test-title" />)
+
+        const wrapper = getByTestId('Collection-Card-Text-Wrapper')
+        expect(wrapper).toBeInTheDocument()
+        expect(wrapper.innerHTML).toContain("test-title")
+
+        const imageIcon = getByTestId('Collection-Card-Image-Icon')
+        expect(imageIcon).toBeInTheDocument()
+        expect(imageIcon).toHaveAttribute("src", "collection-image-o.png")
+
+        expect(getByTestId('tag-layout')).toBeInTheDocument()
+
+    })
+
+})
